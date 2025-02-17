2bdde4e179fdd7e425e0559a0f77d6caddcc487e
TejasChitroda <chitrodatejas176@gmail.com>
2025-02-17 16:07:50 +0530

    DeguddingAlpication completed

--- DebuggingApplication/Program.cs
+++ DebuggingApplication/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace debuggingAplication
+{
+    class Debug
+    {
+        public static void Main(string[] args)
+        {
+            var userResponse = Console.ReadLine();
+            // if we give integer insted of string in that time it will give 
+            if (Convert.ToBoolean(int.Parse(userResponse)))
+            {
+                Console.WriteLine("The entered value is a integer");
+            }
+        }
+    }
+}
